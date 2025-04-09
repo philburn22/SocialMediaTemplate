2fdb5b38ae7f658bc3000739bfb7235f514656f7
philburn22 <143739633+philburn22@users.noreply.github.com>
2025-04-09 15:38:40 -0400

    Add files via upload

--- SocialMediaAccount.h
+++ SocialMediaAccount.h
@@ -3,6 +3,9 @@
 #include <iostream>
 using namespace std;
 
+const int maxFollowed=10;               //global variables
+const int maxFollowers=10;
+
 template <typename T>
 class SocialMediaAccount {
 private:
@@ -13,7 +16,92 @@ private:
     int followedCount;
     bool isPrivate;
 public:
-    SocialMediaAccount():handler(),followerCount(0), followedCount(0), isPrivate(true){}
+    SocialMediaAccount():handler(),followerCount(0), followedCount(0), isPrivate(true){}            //constructors
+    SocialMediaAccount(T handle, bool privacy=true):handler(handle), followerCount(0), followedCount(0), isPrivate(privacy){}
 
+    void setHandler(T handle){handler=handle;}          //setters and getters
+    T getHandler() const{return handler;}
+    void setPrivacy(bool privacy){isPrivate=privacy;}
+    bool getPrivacy() const {return isPrivate;}
+    int getFollowerCount() const {return followerCount;}
+    int getFollowedCount() const{return followedCount;}
+
+    void addFollowed(T user) {             //function that adds a followed acct if possible
+        if (followedCount<maxFollowed) {
+            followed[followedCount++]=user;
+        }else {
+            cout<<"Max limit of followed accounts reached."<<endl;
+        }
+    }
+    void addFollower(T user) {          //function that adds a follower
+        if (followerCount<maxFollowers) {
+            followers[followerCount++]=user;
+        }else {
+            cout<<"Max limit of followers reached."<<endl;
+        }
+    }
+    void displayFollowed()const {       //function that displays followed accounts
+        if (!isPrivate) {
+            cout<<"Following: ";
+            for (int i=0;i<followedCount;i++) {
+                cout<<followed[i];
+                if (i<followedCount-1) {
+                    cout<<", ";
+                }
+            }
+            cout<<endl;
+        }else {
+            cout<<"Access denied."<<endl;
+        }
+    }
+    void displayFollowers() const {             //function that displays followers
+        if (!isPrivate) {
+            cout<<"Followers: ";
+            for (int i=0;i<followerCount;i++) {
+                cout<<followers[i];
+                if (i<followerCount-1) {
+                    cout<<", ";
+                }
+            }
+            cout<<endl;
+        }else {
+            cout<<"Access denied."<<endl;
+        }
+    }
 };
+template <typename T>
+class Twitter:public SocialMediaAccount<T> {
+private:
+    int retweetCount;
+public:
+    Twitter():SocialMediaAccount<T>(), retweetCount(0){}
+    Twitter(T handle, bool privacy=true):SocialMediaAccount<T>(handle, privacy), retweetCount(0){}
+    void setRetweetCount(int count) {           //set and get retweet count
+        retweetCount=count;
+    }
+    int getRetweetCount() const {
+        return retweetCount;
+    }
+    void RT() {
+        retweetCount++;             //increments retweet count by 1
+    }
+};
+template <typename T>
+class Instagram:public SocialMediaAccount<T> {
+private:
+    int likeCount;
+public:
+    Instagram():SocialMediaAccount<T>(), likeCount(0){}         //constructors
+    Instagram(T handle, bool privacy=true):SocialMediaAccount<T>(handle, privacy), likeCount(0){}
+
+    void setLikeCount(int count) {              //getters and setters for the count of "likes"
+        likeCount=count;
+    }
+    int getLikeCount() const {
+        return likeCount;
+    }
+    void Like() {
+        likeCount++;            //increments like count by 1
+    }
+    };
 #endif //SOCIALMEDIAACCOUNT_H
--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
  * Author              : Nathan Philburn
  * Program             : Homework 4
  * Date Created        : 4/3/2025
- * Date Last Modified  : 4/4/2025
+ * Date Last Modified  : 4/9/2025
  * Usage               : No command line arguments.
  *
  * Description:
@@ -12,8 +12,85 @@
  *   the increment functions for retweets and likes.
  *************************************************************************/
 #include "SocialMediaAccount.h"
+#include <iostream>
+#include <string>
 using namespace std;
 
+struct Profile {
+    int userId;
+    string username;
+};
+
+ostream& operator<<(ostream& os, const Profile& profile){os<<"Profile:(ID: "<<profile.userId<<", Username: "<<profile.username<<")";
+    return os;
+}
+
 int main() {
+    Twitter<string> TS("DarthVader", false);  // Public account.
+    TS.addFollower("ObiWan");
+    TS.addFollower("Yoda");
+    TS.addFollowed("Emp_Palpatine");
+    TS.addFollowed("JarJar");
+
+    cout<<"Twitter account name: "<<TS.getHandler()<<endl;
+    cout<<"Who is following DarthVader?"<<endl;
+    TS.displayFollowers();
+    cout<<"Who does DarthVader follow?"<<endl;
+    TS.displayFollowed();
+    cout<<"Initial retweet count: "<<TS.getRetweetCount()<<endl;
+    TS.RT();TS.RT();TS.RT();TS.RT();TS.RT();TS.RT();TS.RT();         //retweets
+    cout<<"Retweet count after "<<TS.getHandler()<<" completed retweeting: "<<TS.getRetweetCount()<<endl;
+    cout<<"=================================="<<endl;
+
+    Profile profile1={1, "PrincessLeia"};       //test for twitter profile types
+    Profile profile2={2, "HanSolo"};
+    Profile profile3={3, "Luke_Skywalker"};
+
+    Twitter<Profile> TP(profile1, false);   //public account
+    TP.addFollower(profile2);
+    TP.addFollower(profile3);
+    TP.addFollowed(profile3);
+
+    cout<<"Twitter profile info: "<<TP.getHandler()<<endl;
+    cout<<"Who follows "<<TP.getHandler()<<"?"<<endl;
+    TP.displayFollowers();
+    cout<<"Who does "<<TP.getHandler()<<" follow?"<<endl;
+    TP.displayFollowed();
+    cout<<"Initial retweet count: "<<TP.getRetweetCount()<<endl;
+    TP.RT();                //retweets
+    cout<<"Retweet count after "<<TP.getHandler()<<" completed retweeting: "<<TP.getRetweetCount()<<endl;
+    cout<<"=================================="<<endl;
+
+    Instagram<string> IS("Rey_Skywalker", false);  //testing instagram with string type
+    IS.addFollower("Finn");
+    IS.addFollower("DarthMaul");
+    IS.addFollowed("R2_D2");
+
+    cout<<"Instagram account name: "<<IS.getHandler()<<endl;
+    cout<<"Who follows "<<IS.getHandler()<<"?"<<endl;
+    IS.displayFollowers();
+    cout<<"Who does "<<IS.getHandler()<<" follow?"<<endl;
+    IS.displayFollowed();
+    cout<<"Initial like count: "<<IS.getLikeCount()<<endl;
+    IS.Like();IS.Like();IS.Like();
+    cout<<"Like count after "<<IS.getHandler()<<" liked post(s): "<<IS.getLikeCount()<<endl;
+    cout<<"=================================="<<endl;
+
+    Profile profile4={4, "Mandalorian"};        //testing instagram with profile
+    Profile profile5={5, "Boba_Fett"};
+
+    Instagram<Profile> IP(profile4, false);
+    IP.addFollower(profile5);
+    IP.addFollowed(profile5);
+
+    cout<<"Instagram profile info:"<<IP.getHandler()<<endl;
+    cout<<"Who follows "<<IP.getHandler()<<"?"<<endl;
+    IP.displayFollowers();
+    cout<<"Who does "<<IP.getHandler()<<" follow?"<<endl;
+    IP.displayFollowed();
+    cout<<"Initial like count: "<<IP.getLikeCount()<<endl;
+    IP.Like();IP.Like();IP.Like();IP.Like();                //likes
+    cout<<"Like count after "<<IP.getHandler()<<"liked post(s): "<<IP.getLikeCount()<<endl;
+
     return 0;
 }
